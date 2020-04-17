e1d37143a4fa0831b9f78eb87d1ee1351a971777
s-alex-smith <salexsmith91@gmail.com>
2020-04-17 12:02:32 +0100

    ensuring up to date for deployment

---
+++ build/precache-manifest.9fed6b32137b5b7343441333a04c9ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787f59b756536874587fe48019247e29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3a49a258fdc669b76f8",
+    "url": "/static/css/main.669cae7d.chunk.css"
+  },
+  {
+    "revision": "ffc9400ec9f9c6abe0cb",
+    "url": "/static/js/2.956475ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.956475ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a49a258fdc669b76f8",
+    "url": "/static/js/main.6b52942e.chunk.js"
+  },
+  {
+    "revision": "e4845b3f5b684abacbba",
+    "url": "/static/js/runtime-main.02739728.js"
+  }
+]);
